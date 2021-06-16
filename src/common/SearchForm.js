a2571dc1ea50e47beab6d343c33d362ce68531ea
rgoshen <rick.goshen@gmail.com>
2021-06-15 19:54:29 -0700

    updated search form placesholder

--- src/common/SearchForm.js
+++ src/common/SearchForm.js
@@ -37,7 +37,7 @@ function SearchForm({ searchFor }) {
         <input
           className="form-control form-control-lg flex-grow-1"
           name="searchTerm"
-          placeholder="Enter search term.."
+          placeholder="Enter search term..."
           value={searchTerm}
           onChange={handleChange}
         />
