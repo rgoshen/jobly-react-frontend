cbb56c2a4e0333634b12b0264e4300210ba1b766
rgoshen <rick.goshen@gmail.com>
2021-06-15 19:53:23 -0700

    fixed serach form button

--- src/common/SearchForm.js
+++ src/common/SearchForm.js
@@ -32,20 +32,20 @@ function SearchForm({ searchFor }) {
   }
 
   return (
-      <div className="SearchForm mb-4">
-        <form className="form-inline" onSubmit={handleSubmit}>
-          <input
-              className="form-control form-control-lg flex-grow-1"
-              name="searchTerm"
-              placeholder="Enter search term.."
-              value={searchTerm}
-              onChange={handleChange}
-          />
-          <button type="submit" className="btn btn-lg btn-primary">
-            Submit
-          </button>
-        </form>
-      </div>
+    <div className="SearchForm mb-4">
+      <form className="form-inline" onSubmit={handleSubmit}>
+        <input
+          className="form-control form-control-lg flex-grow-1"
+          name="searchTerm"
+          placeholder="Enter search term.."
+          value={searchTerm}
+          onChange={handleChange}
+        />
+        <button type="submit" className="btn btn-lg btn-primary mt-2">
+          Submit
+        </button>
+      </form>
+    </div>
   );
 }
 
