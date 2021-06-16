5c27d1f41b79aa99b80961b27e5503707adae553
rgoshen <rick.goshen@gmail.com>
2021-06-15 18:58:05 -0700

    fix homepage styling

--- src/homepage/Homepage.js
+++ src/homepage/Homepage.js
@@ -17,28 +17,29 @@ function Homepage() {
   console.debug("Homepage", "currentUser=", currentUser);
 
   return (
-      <div className="Homepage">
-        <div className="container text-center">
-          <h1 className="mb-4 font-weight-bold">Jobly</h1>
-          <p className="lead">All the jobs in one, convenient place.</p>
-          {currentUser
-              ? <h2>
-                Welcome Back, {currentUser.firstName || currentUser.username}!
-              </h2>
-              : (
-                  <p>
-                    <Link className="btn btn-primary font-weight-bold mr-3"
-                          to="/login">
-                      Log in
-                    </Link>
-                    <Link className="btn btn-primary font-weight-bold"
-                          to="/signup">
-                      Sign up
-                    </Link>
-                  </p>
-              )}
-        </div>
+    <div className="Homepage">
+      <div className="container text-center">
+        <h1 className="mb-4 font-weight-bold">Jobly</h1>
+        <p className="lead">All the jobs in one, convenient place.</p>
+        {currentUser ? (
+          <h2>
+            Welcome Back, {currentUser.firstName || currentUser.username}!
+          </h2>
+        ) : (
+          <p>
+            <Link className="btn btn-primary font-weight-bold mx-1" to="/login">
+              Log in
+            </Link>
+            <Link
+              className="btn btn-primary font-weight-bold mx-1"
+              to="/signup"
+            >
+              Sign up
+            </Link>
+          </p>
+        )}
       </div>
+    </div>
   );
 }
 
