7f9aa57ee4b582babcdf8e99ddf0c82b4e7604a9
rgoshen <rick.goshen@gmail.com>
2021-06-18 14:49:49 -0700

    fix profile alert style

--- src/profiles/ProfileForm.js
+++ src/profiles/ProfileForm.js
@@ -36,11 +36,15 @@ function ProfileForm() {
   // const [saveConfirmed, setSaveConfirmed] = useTimedMessage()
 
   console.debug(
-      "ProfileForm",
-      "currentUser=", currentUser,
-      "formData=", formData,
-      "formErrors=", formErrors,
-      "saveConfirmed=", saveConfirmed,
+    "ProfileForm",
+    "currentUser=",
+    currentUser,
+    "formData=",
+    formData,
+    "formErrors=",
+    formErrors,
+    "saveConfirmed=",
+    saveConfirmed
   );
 
   /** on form submit:
@@ -72,7 +76,7 @@ function ProfileForm() {
       return;
     }
 
-    setFormData(f => ({ ...f, password: "" }));
+    setFormData((f) => ({ ...f, password: "" }));
     setFormErrors([]);
     setSaveConfirmed(true);
 
@@ -83,7 +87,7 @@ function ProfileForm() {
   /** Handle form data changing */
   function handleChange(evt) {
     const { name, value } = evt.target;
-    setFormData(f => ({
+    setFormData((f) => ({
       ...f,
       [name]: value,
     }));
@@ -91,72 +95,71 @@ function ProfileForm() {
   }
 
   return (
-      <div className="col-md-6 col-lg-4 offset-md-3 offset-lg-4">
-        <h3>Profile</h3>
-        <div className="card">
-          <div className="card-body">
-            <form>
-              <div className="form-group">
-                <label>Username</label>
-                <p className="form-control-plaintext">{formData.username}</p>
-              </div>
-              <div className="form-group">
-                <label>First Name</label>
-                <input
-                    name="firstName"
-                    className="form-control"
-                    value={formData.firstName}
-                    onChange={handleChange}
-                />
-              </div>
-              <div className="form-group">
-                <label>Last Name</label>
-                <input
-                    name="lastName"
-                    className="form-control"
-                    value={formData.lastName}
-                    onChange={handleChange}
-                />
-              </div>
-              <div className="form-group">
-                <label>Email</label>
-                <input
-                    name="email"
-                    className="form-control"
-                    value={formData.email}
-                    onChange={handleChange}
-                />
-              </div>
-              <div className="form-group">
-                <label>Confirm password to make changes:</label>
-                <input
-                    type="password"
-                    name="password"
-                    className="form-control"
-                    value={formData.password}
-                    onChange={handleChange}
-                />
-              </div>
+    <div className="col-md-6 col-lg-4 offset-md-3 offset-lg-4">
+      <h3>Profile</h3>
+      <div className="card">
+        <div className="card-body">
+          <form>
+            <div className="form-group">
+              <label>Username</label>
+              <p className="form-control-plaintext">{formData.username}</p>
+            </div>
+            <div className="form-group">
+              <label>First Name</label>
+              <input
+                name="firstName"
+                className="form-control"
+                value={formData.firstName}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-group">
+              <label>Last Name</label>
+              <input
+                name="lastName"
+                className="form-control"
+                value={formData.lastName}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-group">
+              <label>Email</label>
+              <input
+                name="email"
+                className="form-control"
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-group mb-2">
+              <label>Confirm password to make changes:</label>
+              <input
+                type="password"
+                name="password"
+                className="form-control"
+                value={formData.password}
+                onChange={handleChange}
+              />
+            </div>
 
-              {formErrors.length
-                  ? <Alert type="danger" messages={formErrors} />
-                  : null}
+            {formErrors.length ? (
+              <Alert type="danger" messages={formErrors} />
+            ) : null}
 
-              {saveConfirmed
-                  ?
-                  <Alert type="success" messages={["Updated successfully."]} />
-                  : null}
+            {saveConfirmed ? (
+              <Alert type="success" messages={["Updated successfully."]} />
+            ) : null}
 
-              <button
-                  className="btn btn-primary btn-block mt-4"
-                  onClick={handleSubmit}
-              >
-                Save Changes
-              </button>
-            </form>
-          </div>
+            <button
+              className="btn btn-primary btn-block mt-4"
+              onClick={handleSubmit}
+            >
+              Save Changes
+            </button>
+          </form>
         </div>
       </div>
+    </div>
   );
 }
 
